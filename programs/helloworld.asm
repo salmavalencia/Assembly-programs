bec12e8f60e22ebfc398db1a33a44f3f3b2087b5
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-04 17:55:20 -0400

    Create helloworld.asm

---
+++ programs/helloworld.asm
@@ -0,0 +1,13 @@
+SECTION .data
+msg     db      'Hello World!', 0Ah     
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     edx, 13     
+    mov     ecx, msg    
+    mov     ebx, 1      
+    mov     eax, 4      
+    int     80h
