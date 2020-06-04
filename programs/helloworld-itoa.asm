56a5d7764886cd90eca812212a8cc9c1b170d62b
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-04 18:04:56 -0400

    Create helloworld-itoa.asm

---
+++ programs/helloworld-itoa.asm
@@ -0,0 +1,17 @@
+%include        'functions3.asm'
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     ecx, 0
+ 
+nextNumber:
+    inc     ecx
+    mov     eax, ecx
+    call    iprintLF        
+    cmp     ecx, 10
+    jne     nextNumber
+ 
+    call    quit
