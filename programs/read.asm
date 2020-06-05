a8e328944f0435a4dcd2e90670f37d16631949a9
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-05 18:58:55 -0400

    Create read.asm

---
+++ programs/read.asm
@@ -0,0 +1,40 @@
+%include    'functions4.asm'
+ 
+SECTION .data
+filename db 'readme2.txt', 0h    
+contents db 'Hello world!', 0h  
+ 
+SECTION .bss
+fileContents resb 255,          
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     ecx, 0777           
+    mov     ebx, filename
+    mov     eax, 8
+    int     80h
+ 
+    mov     edx, 12             
+    mov     ecx, contents
+    mov     ebx, eax
+    mov     eax, 4
+    int     80h
+ 
+    mov     ecx, 0              
+    mov     ebx, filename
+    mov     eax, 5
+    int     80h
+ 
+    mov     edx, 12             
+    mov     ecx, fileContents   
+    mov     ebx, eax            
+    mov     eax, 3              
+    int     80h                 
+ 
+    mov     eax, fileContents   
+    call    sprintLF            
+ 
+    call    quit                
