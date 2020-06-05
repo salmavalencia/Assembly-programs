661c212d41103198987e2dee0aead7bebf3d43db
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-05 19:16:20 -0400

    Update socket3.asm

--- programs/socket3.asm
+++ programs/socket3.asm
@@ -5,14 +5,14 @@ global  _start
  
 _start:
  
-    xor     eax, eax            ; initialize some registers
+    xor     eax, eax            
     xor     ebx, ebx
     xor     edi, edi
     xor     esi, esi
  
 _socket:
  
-    push    byte 6              ; create socket from lesson 29
+    push    byte 6              
     push    byte 1
     push    byte 2
     mov     ecx, esp
@@ -22,7 +22,7 @@ _socket:
  
 _bind:
  
-    mov     edi, eax            ; bind socket from lesson 30
+    mov     edi, eax            
     push    dword 0x00000000
     push    word 0x2923
     push    word 2
@@ -37,13 +37,13 @@ _bind:
  
 _listen:
  
-    push    byte 1              ; move 1 onto stack (max queue length argument)
-    push    edi                 ; push the file descriptor onto stack
-    mov     ecx, esp            ; move address of arguments into ecx
-    mov     ebx, 4              ; invoke subroutine LISTEN (4)
-    mov     eax, 102            ; invoke SYS_SOCKETCALL (kernel opcode 102)
-    int     80h                 ; call the kernel
+    push    byte 1              
+    push    edi                 
+    mov     ecx, esp            
+    mov     ebx, 4              
+    mov     eax, 102            
+    int     80h                 
  
 _exit:
  
-    call    quit                ; call our quit function
+    call    quit                
