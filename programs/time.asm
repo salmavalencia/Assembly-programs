e7db367eec28f463e3a253d0d7e95c846447e68e
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-06-05 18:54:22 -0400

    Create time.asm

---
+++ programs/time.asm
@@ -0,0 +1,18 @@
+%include        'functions4.asm'
+ 
+SECTION .data
+msg        db      'Seconds since Jan 01 1970: ', 0h     
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     eax, msg        
+    call    sprint          
+ 
+    mov     eax, 13         
+    int     80h             
+ 
+    call    iprintLF        
+    call    quit            
